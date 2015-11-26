0cd90936712208ae7b14e5e67c55c076cde889d8
Stefan <huemer.stefan@gmx.net>
2015-11-26 18:32:58 +0100

    baic args

--- src/Gdir.java
+++ src/Gdir.java
@@ -4,7 +4,22 @@ public class Gdir {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, gdir");
-	
+		
+		if (args.length > 0)
+		{
+			System.out.println("args:");
+			for(int i = 0; i <= args.length - 1; i++)
+			{
+				System.out.print(args[i].substring(1));
+				System.out.print(", ");
+			}
+		}
+		else
+		{
+			System.out.println("keine args");
+		}
+		System.out.println();
+		
 	}
 	
 }
