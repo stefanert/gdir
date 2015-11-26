52aa3f53ceab2a66a48af3f165c17392ee579710
Stefan H <huemer.stefan@gmx.net>
2015-11-26 21:00:09 +0100

    einlesen von datein und simples cleanup

--- src/Gdir.java
+++ src/Gdir.java
@@ -1,24 +1,80 @@
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Gdir {
 	
 	public static void main(String[] args) {
 		
+		int i = 0;
+		String current_arg = "";
+		
 		System.out.println("Hello, gdir");
+		
+		// args verarbeiten
+		
+		// hier werden die optionen wie case folding usw. gespeichert
+		ArrayList<String> options = new ArrayList<String>();
 		
 		if (args.length > 0)
 		{
-			System.out.println("args:");
-			for(int i = 0; i <= args.length - 1; i++)
+			//~ System.out.println("args:");
+			for(i = 0; i <= args.length - 1; i++)
 			{
-				System.out.print(args[i].substring(1));
-				System.out.print(", ");
+				current_arg = args[i];
+				
+				// wenn das aktuelle arg nicht mit einem "-" beginnt wird es uebersprungen
+				// wenn "-query test"
+				//~ if (false == current_arg.startsWith("-"))
+				//~ {
+					//~ System.out.print("contu");
+					//~ continue;
+				//~ }
+				
+				current_arg = current_arg.substring(1);
+				
+				System.out.println(current_arg);
+				
+				// cf = case folding
+				// st = stemming
+				if
+				(
+					(current_arg.equals("cf")) ||
+					(current_arg.equals("st"))
+				)
+				{
+					options.add(current_arg);
+				}
+				else
+				{
+					System.out.println("unbekanntes arg: " + current_arg);
+					return;
+				}
 			}
 		}
 		else
 		{
 			System.out.println("keine args");
+			return;
 		}
-		System.out.println();
+		
+		String path_rel = "../resources/20_newsgroups_subset/";
+		
+		Index index = new Index();
+		index.init(path_rel, options);
+		
+		//~ 
+		//~ File directory_base = new File(path_rel);
+		//~ 
+        //~ //ArrayList<File> subdirectories = new ArrayList<File>();
+        //~ File[] subdirectories = directory_base.listFiles();
+        //~ 
+        //~ for (i=0; i < subdirectories.length ; ++i)
+		//~ {
+			//~ System.out.println(subdirectories[i]);
+		//~ }
+		
+		
 		
 	}
 	
---
+++ src/Index.java
@@ -0,0 +1,127 @@
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Index {
+	
+	private String path;
+	private ArrayList<String> options;
+	
+	public Index()
+	{
+		
+	}
+	
+	public void init(String path, ArrayList<String> options)
+	{
+		// das mit dem pfad und den optionen vielleicht in public Indesx()
+		this.path = path;
+		this.options = options;
+		
+		System.out.println("INIT");
+		
+		System.out.println("path: " + this.path);
+		System.out.println("options: ");
+		for (int i = 0; i < this.options.size(); ++i)
+		{
+			System.out.println(this.options.get(i));
+		}
+		
+		// ordner und unterordner holen
+		File directory_base = new File(this.path);
+        File[] subdirectories = directory_base.listFiles();
+        
+        // arraylist wo alle datein gespeichert werden
+        ArrayList<File> all_files = new ArrayList<File>();
+        
+        File[] current_dir_files;
+        for (int i=0; i < subdirectories.length ; ++i)
+		{
+			// datein des aktuellen ordners
+			current_dir_files = subdirectories[i].listFiles();
+			
+			// datein des aktuellen ordners durchwandern
+			for (int j = 0; j < current_dir_files.length; ++j)
+			{
+				all_files.add(current_dir_files[j]);
+			}
+		}
+		
+		System.out.println("========================================");
+		
+		ArrayList<String> usefull_lines = get_usefull_lines(all_files.get(4));
+		
+		for (int i = 0; i < usefull_lines.size(); ++i)
+		{
+			System.out.println(usefull_lines.get(i));
+		}
+		
+		
+		System.out.println("========================================");
+		
+	}
+	
+	public ArrayList<String> get_usefull_lines(File file)
+	{
+		ArrayList<String> usefull_lines = new ArrayList<String>();
+		
+		InputStream is = null; 
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+		
+		try
+		{
+			is = new FileInputStream(file);
+			isr = new InputStreamReader(is);
+			br = new BufferedReader(isr);
+			
+			boolean still_reading_header = true;
+			
+			for (String line = br.readLine(); line != null; line = br.readLine())
+			{
+				// wenn ich noch dabei bin den header zu lesen
+				if (still_reading_header == true)
+				{
+					// wenn in der zeile ein ": " vorkommt ist es
+					// teil des headers und wird ignoriert
+					if (line.contains(": "))
+					{
+						continue;
+					}
+					// es wird davon ausgegangen, dass der header zu ende ist
+					else
+					{
+						still_reading_header = false;
+					}
+				}
+				
+				// die ueberpruefung kann eigentlich weggelassen werden
+				if (still_reading_header == false)
+				{
+					// leere zeielen werden uebersprungen
+					if (line.equals("") == false)
+					{
+						usefull_lines.add(line);
+					}
+				}
+				
+			}
+			
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+		
+		return usefull_lines;
+	}
+	
+}
